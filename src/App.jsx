efe4f7c5cd82eb03c1b33818814d2cb09caac4eb
arju88nair <arju88nair@gmail.com>
2018-07-24 22:11:36 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React ,{ Component }from 'react';
+export default class App  extends React.Component{
+ render(){
+  return(
+   <div>
+    <h1>I'm the component</h1>
+   </div>
+  );
+ }
+}
