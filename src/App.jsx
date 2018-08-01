5578a1e42f0aae932801d9c72ea8e146f39812bb
arju88nair <arju88nair@gmail.com>
2018-08-01 22:01:26 +0530

    completed countdowm

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,43 @@
-import React,{ Component }from 'react';
-import './App.css'
-import Clock from './Clock'
-export default class App  extends React.Component{
-    constructor(props)
- 	{
- 		super(props);
- 		this.state={
-             deadline:"July 30,2018",
-             newDeadline:''
-         }
+import React, { Component } from 'react';
+import Clock from './Clock';
+import './App.css';
+import { Form, FormControl, Button } from 'react-bootstrap';
 
-     }
-     
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'December 25, 2017',
+      newDeadline: ''
+    }
+  }
 
-     onchangedeadline()
-     {
-         this.setState({deadline:this.state.newDeadline})
-     }
- render(){
-  return(
-   <div className="App">
-   <div className="App-title">Coundown to {this.state.deadline}</div>
-  <Clock deadline={this.state.deadline}/>
-   <div>
-   	<input placeholder="Here fucker!"onChange={event => this.setState({newDeadline:event.target.value})}></input>
-   	<button onClick={()=> this.onchangedeadline()}>Blah</button>
-   </div>
-   </div>
-  );
- }
+  changeDeadline() {
+    this.setState({deadline: this.state.newDeadline});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-title">
+          Countdown to {this.state.deadline}
+        </div>
+        <Clock
+          deadline={this.state.deadline}
+        />
+        <Form inline>
+          <FormControl
+            className="Deadline-input"
+            placeholder='new date'
+            onChange={event => this.setState({newDeadline: event.target.value})}
+          />
+          <Button onClick={() => this.changeDeadline()}>
+            Submit
+          </Button>
+        </Form>
+      </div>
+    )
+  }
 }
+
+export default App;
--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,40 +1,49 @@
 import React, { Component } from 'react';
-import './App.css'
+import './App.css';
 
-class Clock extends React.Component {
-    constructor(props)
-    {
-        super(props);
-        this.state={
-Days:0,
-Hours:0,
-Minutes:0,
-Seconds:0
-        }
-
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0
     }
+  }
 
-    getTimeUntil(deadline){
-        const time=Date.parse(deadline)-Date.parse(new Date())
-        const seconds = Math.floor((time/1000) % 60)
-        const minutes = Math.floor((time/1000/60) % 60)
-        const hours = Math.floor((time/(1000*60*60)) % 24)
-        const days = Math.floor((time/(1000*60*60*24)))
-        console.log(seconds+"Sec"+minutes+"min"+hours+"hours"+days)
-    }
+  componentWillMount() {
+    this.getTimeUntil(this.props.deadline);
+  }
 
+  componentDidMount() {
+    setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
+  }
 
-    render() {
-        this.getTimeUntil(this.props.deadline)
-        return (
-            <div>
-                <div className="clock-Days">{this.state.Days} days</div>
-                <div className="clock-Hours">{this.state.Hours} Hours</div>
-                <div className="clock-Minutes">{this.state.Minutes} Minutes</div>
-                <div className="clock-Seconds">{this.state.Seconds} Seconds</div>
-            </div>
-        );
-    }
+  leading0(num) {
+    return num < 10 ? '0' + num : num;
+  }
+
+  getTimeUntil(deadline) {
+    const time = Date.parse(deadline) - Date.parse(new Date());
+    const seconds = Math.floor((time/1000) % 60);
+    const minutes = Math.floor((time/1000/60) % 60);
+    const hours = Math.floor(time/(1000*60*60) % 24);
+    const days = Math.floor(time/(1000*60*60*24));
+
+    this.setState({days, hours, minutes, seconds});
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="Clock-days">{this.leading0(this.state.days)} days</div>
+        <div className="Clock-hours">{this.leading0(this.state.hours)} hours</div>
+        <div className="Clock-minutes">{this.leading0(this.state.minutes)} minutes</div>
+        <div className="Clock-seconds">{this.leading0(this.state.seconds)} seconds</div>
+      </div>
+    )
+  }
 }
 
-export default Clock
+export default Clock;
